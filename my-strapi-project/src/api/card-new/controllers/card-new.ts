8f5345b45a03f62795bacfdf6f8e084ac3ad7ced
Rupesh Karale <rupeshkarale3245@gmail.com>
2024-10-06 18:19:12 +0530

    changes

--- my-strapi-project/src/api/card-new/controllers/card-new.ts
+++
@@ -1,7 +0,0 @@
-/**
- * card-new controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::card-new.card-new');
--- my-strapi-project/src/api/card-new/services/card-new.ts
+++
@@ -1,7 +0,0 @@
-/**
- * card-new service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::card-new.card-new');
